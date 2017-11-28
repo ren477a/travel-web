85265107a97b1afe17507dfa2dda0c9478c4800f
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-11-28 20:38:20 +0800

    Moved secret

--- config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  database: 'mongodb://dev:dev@ds155325.mlab.com:55325/tdw',
-  secret: 'blacksquad'
-};
--- config/passport.js
+++ config/passport.js
@@ -1,13 +1,12 @@
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const User = require('../models/user');
-const config = require('../config/database');
 
 module.exports = function(passport) {
   let opts = {};
   opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
   console.log(opts.jwtFromRequest);
-  opts.secretOrKey = config.secret;
+  opts.secretOrKey = process.env.JWT_SECRET;
   passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
     console.log(jwt_payload);
     User.getUserById(jwt_payload.data._id, (err, user) => {
