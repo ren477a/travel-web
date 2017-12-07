3473362943d8378e33e28df1b0016d061a12129a
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-12-07 18:57:40 +0800

    created file

---
+++ routes/payment-routes.js
@@ -0,0 +1,25 @@
+const braintree = require('braintree');
+const router = express.Router(); // eslint-disable-line new-cap
+const gateway = require('../config/gateway');
+
+const TRANSACTION_SUCCESS_STATUSES = [
+    braintree.Transaction.Status.Authorizing,
+    braintree.Transaction.Status.Authorized,
+    braintree.Transaction.Status.Settled,
+    braintree.Transaction.Status.Settling,
+    braintree.Transaction.Status.SettlementConfirmed,
+    braintree.Transaction.Status.SettlementPending,
+    braintree.Transaction.Status.SubmittedForSettlement
+];
+
+app.get("/client_token", function (req, res) {
+    gateway.clientToken.generate({}, function (err, response) {
+        res.send(response.clientToken);
+    });
+});
+
+router.post('/', (req, res, next) => {
+
+});
+
+module.exports = router;
