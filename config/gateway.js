93b640e55a8961f8210f3b8820eff63bca549e63
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-12-07 18:41:38 +0800

    config for braintree

---
+++ config/gateway.js
@@ -0,0 +1,14 @@
+const braintree = require('braintree');
+const environment, gateway;
+
+require('dotenv').config();
+environment = process.env.BT_ENVIRONMENT.charAt(0).toUpperCase() + process.env.BT_ENVIRONMENT.slice(1);
+
+gateway = braintree.connect({
+  environment: braintree.Environment[environment],
+  merchantId: process.env.BT_MERCHANT_ID,
+  publicKey: process.env.BT_PUBLIC_KEY,
+  privateKey: process.env.BT_PRIVATE_KEY
+});
+
+module.exports = gateway;
