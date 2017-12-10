71d5a48a720d8daaa053b1a998f6b85016ef02ea
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-12-10 15:22:26 +0800

    charge endpoint

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -1,8 +1,22 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
-
+const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
 
 router.post('/charge', (req, res, next) => {
     console.log(req.body);
+    // Charge client
+    // Save transaction to database
+    stripe.charges.create({
+        amount: req.body.amount,
+        currency: "php",
+        description: "Example charge",
+        source: req.body.token.id,
+    }, (err, charge) => {
+        if(err) {
+            res.json({success: false, charge: null});
+        } else {
+            res.json({success: true, charge: charge});
+        }
+    });
 });
 
 
