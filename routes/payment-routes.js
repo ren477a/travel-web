5dd14a19eb92d3c972fdbec7e287bf5d0bd37592
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2017-12-11 18:49:46 +0800

    added transaction endpoint

--- routes/payment-routes.js
+++ routes/payment-routes.js
@@ -1,24 +1,48 @@
 const router = require('express').Router(); // eslint-disable-line new-cap
 const stripe = require('stripe')(process.env.STRIPE_PRIVATE_KEY);
+const Transaction = require('../models/transaction');
+const voucher = require('voucher-code-generator');
 
 router.post('/charge', (req, res, next) => {
-    console.log(req.body);
-    // Charge client
-    // Save transaction to database
     stripe.charges.create({
         amount: req.body.amount,
         currency: "php",
         description: "Example charge",
         source: req.body.token.id,
     }, (err, charge) => {
-        if(err) {
-            res.json({success: false, charge: null});
+        if (err) {
+            res.json({ success: false, charge: null, err: err });
         } else {
-            res.json({success: true, charge: charge});
+            res.json({ success: true, charge: charge, err: null });
+            // Generate voucher codes
+            // Save to database
+            // Email receipt
+
         }
     });
 });
 
+router.post('/transaction', (req, res, next) => {
+    let prefix = req.body.tourId.substr(req.body.tourId.length - 4) + "-";
+    let data = req.body;
+    data.voucherCodes = voucher.generate({
+        length: 6,
+        count: req.body.quantity,
+        charset: 'alphanumeric',
+        prefix: prefix
+    });
+    console.log(data);
+    let newTransaction = new Transaction(data);
+
+    Transaction.addTransaction(newTransaction, (err, transaction) => {
+        if (err) {
+            console.log(err);
+            res.json({ success: false, transaction: null });
+        } else {
+            res.json({ success: true, transaction: transaction });
+        }
+    });
+});
 
 
 router.get('/', (req, res, next) => {
