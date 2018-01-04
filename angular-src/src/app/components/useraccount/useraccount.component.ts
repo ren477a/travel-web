acf86bd0b5a291745d815502bfdd925ecf75a2a8
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-04 10:38:14 +0800

    added file

--- angular-src/src/app/components/useraccount/useraccount.component.ts
+++ angular-src/src/app/components/useraccount/useraccount.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TransactionService } from '../../services/transaction.service';
 import { AuthService } from '../../services/auth.service';
+import { ToursService } from '../../services/tours.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -12,9 +13,11 @@ export class UseraccountComponent implements OnInit {
 
   userTransactions: Array<any>;
 
+
   constructor(
     private transactionService: TransactionService,
     private authService: AuthService,
+    private toursService: ToursService,
     private router: Router
   ) { }
 
@@ -25,6 +28,18 @@ export class UseraccountComponent implements OnInit {
         this.userTransactions = res;
       });
     }
+  }
+
+  stringAsDate(dateStr: string) {
+    return new Date(dateStr);
+  }
+
+  getTourTitle(id) {
+    let tour: any;
+    this.toursService.findTourById(id).subscribe(res => {
+      tour = res;
+      return tour.title;
+    });
   }
 
 }
