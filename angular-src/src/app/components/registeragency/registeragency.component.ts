a53ac7b61cd0fa699a61e4872abf1021fae73484
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-09 10:41:56 +0800

    add photo upload

--- angular-src/src/app/components/registeragency/registeragency.component.ts
+++ angular-src/src/app/components/registeragency/registeragency.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ValidateService } from '../../services/validate.service';
 import { AuthService } from '../../services/auth.service';
+import { ToursService } from '../../services/tours.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,45 +17,83 @@ export class RegisteragencyComponent implements OnInit {
   email: String;
   password: String;
   address: String;
-  dti: String;
-  business: String;
-  bir: String;
+  dti: File;
+  business: File;
+  bir: File;
+
+  msg: String;
 
   constructor(
     private validateService: ValidateService,
     private authService: AuthService,
+    private toursService: ToursService,
     private router: Router
-  ) { }
+  ) { this.msg = ''; }
 
   ngOnInit() {
   }
 
+  getDtiFiles(event) {
+    this.dti = event.target.files[0];
+  }
+  getBusinessFiles(event) {
+    this.business = event.target.files[0];
+  }
+  getBirFiles(event) {
+    this.bir = event.target.files[0];
+  }
+
   onRegisterClick() {
-    console.log("register btn pressed");
-    const agency = {
-      agencyName: this.agencyName, 
-      ownedBy: this.ownedBy, 
-      mobileNumber: this.mobileNumber, 
-      email: this.email, 
-      password: this.password, 
-      address: this.address, 
-      dti: this.dti, 
-      business: this.business, 
-      bir: this.bir
-    }
+    console.log("register agency")
 
-    //Validate Here
+    let dtiStr: String;
+    let businessStr: String;
+    let birStr: String;
+    this.msg = 'Uploading dti file...';
+    this.toursService.uploadPhoto(this.dti).subscribe(res => {
+      if (res.file) {
 
-    this.authService.registerAgency(agency).subscribe(data => {
-      if (data.success) {
-        console.log("Agency registered");
-        this.router.navigate(['/login/agency']);
-      } else {
-        console.log("Something went wrong");
-        this.router.navigate(['/register/agency']);
+        this.msg = 'Uploading business file...';
+        dtiStr = res.file;
+
+        this.toursService.uploadPhoto(this.business).subscribe(res => {
+          if (res.file) {
+            this.msg = 'Uploading bir file...';
+            businessStr = res.file;
+            this.toursService.uploadPhoto(this.bir).subscribe(res => {
+              if (res.file) {
+                this.msg = 'All files uploaded. Please wait...';
+                birStr = res.file;
+                console.log("register btn pressed");
+                const agency = {
+                  agencyName: this.agencyName,
+                  ownedBy: this.ownedBy,
+                  mobileNumber: this.mobileNumber,
+                  email: this.email,
+                  password: this.password,
+                  address: this.address,
+                  dti: dtiStr,
+                  business: businessStr,
+                  bir: birStr
+                }
+
+                //Validate Here
+                console.log(agency);
+                this.authService.registerAgency(agency).subscribe(data => {
+                  if (data.success) {
+                    console.log("Agency registered");
+                    this.router.navigate(['/login/agency']);
+                  } else {
+                    console.log("Something went wrong");
+                    this.router.navigate(['/register/agency']);
+                  }
+                });
+              }
+            })
+          }
+        })
       }
-    });
-    
+    })
   }
 
 }
