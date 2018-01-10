b51a017c67ffa5d303fdb187b0174d8706ccde62
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-10 22:30:37 +0800

    Modified cashout

--- routes/cashout-routes.js
+++ routes/cashout-routes.js
@@ -3,32 +3,75 @@ const passport = require('passport');
 const jwt = require('jsonwebtoken');
 
 const Cashout = require('../models/cashout');
+const Agency = require('../models/agency');
+const Transaction = require('../models/transaction');
 
 // new checkout
 router.post('/', (req, res, next) => {
     // Edit this
-    let newCashout = new Cashout({
-        agencyId: req.body.agencyId,
-        agency: req.body.agency,
-        bankAccount: {
-            accountNumber: req.body.accountNumber,
-            accountName: req.body.accountNumber,
-            backName: req.body.bankAccount
-        },
-        msg: '',
-        status: 'pending',
-        amount: req.body.amount
-        //transactions: req.body.transactions
-    });
-  
-    Cashout.addCashout(newCashout, (err, cashout) => {
-      if (err) {
-        console.log(err);
-        res.json({ success: false, cashout: null });
-      } else {
-        res.json({ success: true, cashout: cashout });
-      }
-    });
-  });
+
+
+    let conditions = { agency: req.body.agency, cashedOut: false }
+        , update = { cashedOut: true }
+        , options = { multi: true };
+
+    Transaction.find(conditions, '_id', (err, doc) => {
+        let transactions = doc.map(val => val._id);
+        console.log(transactions);
+        Transaction.update(conditions, update, options, (err, numAffected) => {
+            if (err) {
+                console.log(err);
+                res.json({ success: false, cashout: null });
+            }
+            else {
+                console.log({
+                    action: "Mark cashouts",
+                    numAffected: numAffected
+                });
+                let conditions2 = { _id: req.body.agencyId }
+                    , update2 = { balance: 0 }
+                    , options2 = { multi: true };
+                Agency.update(conditions2, update2, options2, (err2, numAffected2) => {
+                    if (err2) {
+                        console.log(err2);
+                        res.json({ success: false, cashout: null });
+                    }
+                    else {
+                        console.log({
+                            action: "Subract cashout to balance",
+                            numAffected2: numAffected2
+                        })
+                        let newCashout = new Cashout({
+                            agencyId: req.body.agencyId,
+                            agency: req.body.agency,
+                            bankAccount: {
+                                accountNumber: req.body.accountNumber,
+                                accountName: req.body.accountNumber,
+                                backName: req.body.bankAccount
+                            },
+                            msg: '',
+                            status: 'pending',
+                            amount: req.body.amount,
+                            transactions: transactions
+                        });
+                        Cashout.addCashout(newCashout, (err, cashout) => {
+                            if (err) {
+                                console.log(err);
+                                res.json({ success: false, cashout: null });
+                            } else {
+                                res.json({ success: true, cashout: cashout });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    })
+
+
+
+
+
+});
 
 module.exports = router;
