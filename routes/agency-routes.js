28327c5539611ef69a550ce91efaf21f161487c0
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-14 21:00:02 +0800

    Add file

---
+++ routes/agency-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const agencyController = require('../controllers/agency.controller')
+
+router.get('/', agencyController.readAll)
+
+router.get('/:id', agencyController.read)
+
+module.exports = router;
