a5f937f694e16ed4d0cad40d0ac8d65903c7aea8
Efren Mercado Jr <mercado.efrenjr@gmail.com>
2018-01-14 12:50:41 +0800

    Refactor

---
+++ controllers/tour.controller.js
@@ -0,0 +1,149 @@
+const Tour = require('../models/tour')
+const upload = require('../config/upload')
+
+exports.create = async (req, res) => {
+    let tour = new Tour(req.body)
+    try {
+        let t = await tour.save(req.body)
+        res.json({ tour: t })
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+}
+
+exports.read = async (req, res) => {
+    try {
+        let tour = await Tour.findById(req.params.id).lean()
+        tour.img = await upload.getUrl(tour.img)
+        if (!tour) {
+            res.status(400).json({ error: 'No tour with the given ID' })
+        } else {
+            res.json({ tour: tour })
+        }
+    } catch (err) {
+        res.status(500).json({ error: err })
+    }
+}
+
+exports.readAll = async (req, res) => {
+    try {
+        let query
+        if (req.query.key === undefined &&
+            req.query.min === undefined &&
+            req.query.max === undefined &&
+            req.query.type === undefined) {
+            // RETURN ALL
+            query = {};
+        } else {
+            // SEARCH QUERIES
+            query = { $and: [] };
+            let keySearch = {};
+
+            // KEYWORD
+            if (req.query.key !== undefined) {
+                let keyRegEx = new RegExp('.*' + req.query.key + '.*', 'i');
+                keySearch = { $or: [{ title: keyRegEx }, { description: keyRegEx }, { itinerary: keyRegEx }] };
+                query['$and'].push(keySearch);
+            }
+
+            // PRICE RAANGE
+            if (req.query.min !== undefined && req.query.max !== undefined) {
+                let min = req.query.min;
+                let max = req.query.max;
+                if (min < 0 || max > 1000000) {
+                    res.status(400).send({ error: 'Invalid price range.' });
+                } else {
+                    let range = {};
+                    range['price'] = { $gte: min, $lte: max }
+                    query['$and'].push(range);
+                    console.log(range)
+                }
+            }
+
+            // TYPE
+            if (req.query.type !== undefined) {
+                let type = req.query.type;
+                if (type === 'International' || type === 'Local') {
+                    query['$and'].push({ type: type });
+                } else {
+                    res.status(400).send({ error: 'Invalid type' })
+                }
+            }
+        }
+        console.log(query);
+
+        // PAGINATE
+        let count = await Tour.count(query);
+        totalPages = Math.ceil(count / 9);
+        if (totalPages == 0) {
+            res.status(204).send();
+        }
+        let tours
+        if (req.query.page) {
+            tours = await Tour.find(query).limit(9).skip((req.query.page - 1) * 9).lean()
+        } else {
+            tours = await Tour.find(query).lean()
+        }
+
+        if (!tours) {
+            res.status(400).json({ error: 'No tours with the given query.' })
+        } else {
+            // Fetch image signed URL
+            await Promise.all(tours.map(async (tour) => {
+                tour.img = await upload.getUrl(tour.img)
+            }))
+            res.json({ tours: tours })
+        }
+    } catch (err) {
+        err => res.status(500).json({ error: err })
+    }
+}
+
+exports.update = async (req, res) => {
+    try {
+        let tour = await Tour.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        console.log(tour)
+        if (!tour) {
+            res.status(400).json({ error: 'No tour with the given ID' });
+        } else {
+            res.json({ tour: tour });
+        }
+    } catch (err) {
+        err => res.status(500).json({ error: err })
+    }
+}
+
+exports.delete = async (req, res) => {
+    try {
+        let tour = await Tour.findByIdAndRemove(req.params.id);
+        if (!tour) {
+            res.status(400).json({ error: 'No tour with the given ID' });
+        } else {
+            res.json({ tour: tour });
+        }
+    } catch (err) {
+        err => res.status(500).json({ error: err })
+    }
+}
+
+exports.upload = async (req, res) => {
+    upload.setDestination('tours');
+    upload.single('photo')(req, res, (err) => {
+        if (err) {
+            res.json({
+                msg: err
+            });
+        } else {
+            if (req.file == undefined) {
+                res.status(500).json({
+                    err: 'No File Selected!'
+                });
+            } else {
+                res.json({
+                    msg: 'File Uploaded!',
+                    file: `${req.file.key}`
+                });
+            }
+        }
+    });
+}
--- routes/tour-routes.js
+++ routes/tour-routes.js
@@ -1,187 +1,23 @@
-const router = require('express').Router();
-const passport = require('passport');
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
+const router = require('express').Router()
+const passport = require('passport')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
 
-const Tour = require('../models/tour');
+const Tour = require('../models/tour')
+const tourController = require('../controllers/tour.controller')
 
 const myBucket = 'travelcatalog';
+const upload = require('../config/upload')
+const s3 = require('../config/upload').s3
 
-router.post('/', (req, res, next) => {
-  // Edit this
-  let newTour = new Tour({
-    title: req.body.title,
-    agency: req.body.agency,
-    description: req.body.description,
-    duration: req.body.duration,
-    type: req.body.type,
-    itinerary: req.body.itinerary,
-    inclusions: req.body.inclusions,
-    exclusions: req.body.exclusions,
-    notes: req.body.notes,
-    terms: req.body.terms,
-    validityInDays: req.body.validityInDays,
-    pricing: {
-      ptype: req.body.pricing.ptype,
-      fixed: req.body.pricing.fixed,
-      group: [{
-        persons: req.body.pricing.persons,
-        price: req.body.pricing.price
-      }]
-    },
-    status: 'pending',
-    img: req.body.img,
-    sold: 0
-  });
+router.get('/', tourController.readAll)
 
-  Tour.addTour(newTour, (err, tour) => {
-    if (err) {
-      console.log(err);
-      res.json({ success: false, tour: null });
-    } else {
-      res.json({ success: true, tour: tour });
-    }
-  });
-});
+router.post('/', tourController.create)
 
-const upload = require('../config/upload');
-const s3 = require('../config/upload').s3;
-router.post('/upload', (req, res) => {
-  upload.setDestination('tours');
-  upload.single('photo')(req, res, (err) => {
-    if (err) {
-      res.json({
-        msg: err
-      });
-    } else {
-      if (req.file == undefined) {
-        res.json({
-          msg: 'Error: No File Selected!'
-        });
-      } else {
-        res.json({
-          msg: 'File Uploaded!',
-          file: `${req.file.key}`
-        });
-      }
-    }
-  });
-});
+router.get('/:id', tourController.read)
 
-router.get('/', (req, res, next) => {
-  // Edit this
-  //let query = JSON.parse(req.query.q);
-  //   const url = s3.getSignedUrl('getObject', {
-  //     Bucket: 'travelcatalog',
-  //     Key: myKey,
-  //     Expires: signedUrlExpireSeconds
-  // })
-  console.log(req.query.q);
-  Tour.find().then(tours => {
-    res.send(tours);
-  });
-});
+router.put('/:id', tourController.update)
 
-router.post('/search', (req, res, next) => {
-  let query = req.body.query;
-  let pageNum = req.body.pageNum;
-  let totalPages = 1;
-  console.log('skip ' + ((pageNum - 1) * 9));
-  console.log(query.title);
-  if (query.title != undefined) {
-    query.title = {
-      $regex: new RegExp('.*' + query.title + '.*', 'i')
-    }
-  }
+router.post('/upload', tourController.upload)
 
-  if(query.minPrice && query.maxPrice) {
-    query['pricing.fixed'] = { $gte: query.minPrice, $lte: query.maxPrice }
-    delete query.minPrice;
-    delete query.maxPrice;
-  }
-  console.log(query)
-
-  let sortBy = query.sortBy;
-  if(sortBy) {
-      delete query.sortBy;
-  }
-  console.log(query);
-  Tour.count(query, (err, c) => {
-    console.log(c);
-    totalPages = Math.ceil(c / 9);
-    if (totalPages == 0) {
-      res.json({
-        tours: [],
-        pageNum: 1,
-        totalPages: totalPages
-      })
-    }
-    console.log(totalPages);
-    console.log('total pages ' + totalPages);
-    console.log('pagenum ' + pageNum)
-    if (pageNum > totalPages) pageNum = totalPages;
-    console.log(pageNum)
-    let promise = Tour.find(query).limit(9).skip((pageNum - 1) * 9);
-    console.log(sortBy)
-    if(sortBy) {
-      if(sortBy.col === 'price') {
-        console.log({'pricing.fixed': sortBy.asc});
-        promise = promise.sort({'pricing.fixed': sortBy.asc});
-      } else if (sortBy.col === 'title') {
-        promise = promise.sort({title: sortBy.asc});
-      }
-    }
-    promise.then(tours => {
-      var arrayLength = tours.length;
-      console.log('length ' + arrayLength)
-      let urls = new Array(arrayLength);
-      for (var i = 0; i < arrayLength; i++) {
-        urls[i] = upload.getUrl(tours[i].img)
-      }
-      res.json({
-        tours: tours,
-        pageNum: pageNum,
-        totalPages: totalPages,
-        urls: urls
-      });
-    });
-  });
-
-});
-
-router.get('/featured', (req, res, next) => {
-  Tour.find().limit(9).then(tours => {
-    var arrayLength = tours.length;
-    console.log('length ' + arrayLength)
-    let urls = new Array(arrayLength);
-    for (var i = 0; i < arrayLength; i++) {
-      urls[i] = upload.getUrl(tours[i].img)
-    }
-    res.json({
-      tours: tours,
-      urls: urls
-    });
-  });
-});
-
-router.get('/:id', (req, res, next) => {
-
-  Tour.findOne({ _id: req.params.id }).then(tour => {
-    console.log(tour);
-    let url = upload.getUrl(tour.img);
-    res.json({ tour: tour, imgUrl: url });
-  });
-});
-
-router.put('/archive/:id', (req, res, next) => {
-  var conditions = { _id: req.params.id }
-    , update = { status: 'notonsale' }
-    , options = { multi: true };
-
-  Tour.update(conditions, update, options, (err, numAffected) => {
-    // numAffected is the number of updated documents
-    res.json({ numAffected: numAffected });
-  });
-});
-
-module.exports = router;
+module.exports = router
